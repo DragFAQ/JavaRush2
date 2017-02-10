39d3bc0eb508c658f95fbdc39382da989972a365
Drag <drag.faq@gmail.com>
2017-02-11 00:07:32 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -20,7 +20,15 @@ public class Arkanoid {
     }
 
     public void move() {
+        ball.move();
+        stand.move();
+    }
 
+    public void draw(Canvas canvas) {
+        ball.draw(canvas);
+        stand.draw(canvas);
+        for (Brick b : bricks)
+            b.draw(canvas);
     }
 
     public Ball getBall() {
