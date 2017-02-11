dbc6ccc6c4b8ccdb18c4524d03d07c1d6dbc6c29
Drag <drag.faq@gmail.com>
2017-02-11 22:31:56 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
@@ -6,6 +6,22 @@ Switch для нитей
 public class Solution {
     public static void processThreads(Thread... threads) {
         //implement this method - реализуйте этот метод
+        for (Thread t : threads)
+            switch (t.getState()) {
+                case NEW:
+                    t.start();
+                    break;
+                case WAITING:
+                case TIMED_WAITING:
+                case BLOCKED:
+                    t.interrupt();
+                    break;
+                case RUNNABLE:
+                    t.isInterrupted();
+                    break;
+                case TERMINATED:
+                    System.out.println(t.getPriority());
+            }
     }
 
     public static void main(String[] args) {
