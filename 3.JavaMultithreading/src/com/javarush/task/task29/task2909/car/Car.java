58ab4131bfa3dca0465a9d92e458e95ba062b5a4
Drag <drag.faq@gmail.com>
2017-02-13 21:56:31 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
@@ -41,24 +41,28 @@ public class Car {
     }
 
     public boolean isSummer(Date date , Date summerStart, Date summerEnd) {
-
+        return summerStart.compareTo(date) * date.compareTo(summerEnd) >= 0;
     }
 
-    public int fill(double numberOfLiters) throws Exception {
+    public double getWinterConsumption(int length) {
+        return length * winterFuelConsumption + winterWarmingUp;
+    }
+
+    public double getSummerConsumption(int length) {
+        return length * summerFuelConsumption;
+    }
+
+    public void fill(double numberOfLiters) throws Exception {
         if (numberOfLiters < 0)
             throw new Exception();
         fuel += numberOfLiters;
-        return 0;
     }
 
     public double getTripConsumption(Date date, int length, Date SummerStart, Date SummerEnd) {
-        double consumption;
-        if (date.before(SummerStart) || date.after(SummerEnd)) {
-            consumption = length * winterFuelConsumption + winterWarmingUp;
-        } else {
-            consumption = length * summerFuelConsumption;
-        }
-        return consumption;
+        if (isSummer(date, SummerStart, SummerEnd))
+            return getSummerConsumption(length);
+        else
+            return getWinterConsumption(length);
     }
 
     public int getNumberOfPassengersCanBeTransferred() {
