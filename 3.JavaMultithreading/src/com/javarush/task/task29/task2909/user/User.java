681da2caec0b69efe909f4674cadbdb80528d1cd
Drag <drag.faq@gmail.com>
2017-02-13 23:31:08 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -4,10 +4,9 @@ public class User {
     private String name;
     private String surname;
     private int age;
+    private boolean man;
 
-    private String country;
-    private String city;
-    private House house;
+    private Address address = new Address();
 
     private Work work;
 
@@ -15,6 +14,7 @@ public class User {
         this.name = name;
         this.surname = surname;
         this.age = age;
+        this.man = man;
     }
 
     public void printInfo() {
@@ -27,6 +27,14 @@ public class User {
             System.out.println("Пользователь моложе 16 лет");
         else
             System.out.println("Пользователь старше 16 лет");
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
     }
 
     public String getName() {
@@ -54,23 +62,23 @@ public class User {
     }
 
     public String getCountry() {
-        return country;
+        return address.getCountry();
     }
 
     public void setCountry(String country) {
-        this.country = country;
+        address.setCountry(country);
     }
 
     public String getCity() {
-        return city;
+        return address.getCity();
     }
 
     public void setCity(String city) {
-        this.city = city;
+        address.setCity(city);
     }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
     }
 
     public Work getWork() {
@@ -79,5 +87,9 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public String getBoss() {
+        return work.getBoss();
     }
 }
