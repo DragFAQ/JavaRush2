93bb895abee64bb8316e82e084c395ffb2fffce0
Drag <drag.faq@gmail.com>
2017-02-13 17:39:17 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -11,13 +11,9 @@ public class Human implements Alive {
     protected String name;
     private List<Human> children = new ArrayList<>();
 
-    protected int[] size;
+    protected Size size;
 
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
     public List<Human> getChildren() {
         return Collections.unmodifiableList(children);
@@ -41,11 +37,11 @@ public class Human implements Alive {
             children.remove(human);
     }
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
 
-    public int getBloodGroup() {
+    public BloodGroup getBloodGroup() {
         return bloodGroup;
     }
 
@@ -80,6 +76,11 @@ public class Human implements Alive {
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
+    }
+
+    public static class Size {
+        public int height;
+        public int weight;
     }
 }
