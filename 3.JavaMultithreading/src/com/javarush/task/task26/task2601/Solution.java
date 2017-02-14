90d84ea8138e035aa0c10478a69269c3bec99678
Drag <drag.faq@gmail.com>
2017-02-14 14:45:40 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.task.task26.task2601;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -11,28 +10,29 @@ import java.util.Comparator;
 public class Solution {
 
     public static void main(String[] args) {
-        for (Integer i : sort(new Integer[]{13, 8, 15, 5, 17, 11}))
-            System.out.println(i);
     }
 
     public static Integer[] sort(Integer[] array) {
         //implement logic here
-        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(array));
+        Arrays.sort(array);
+        final double median;
+        if (array.length % 2 == 0)
+            median = ((double) array[array.length / 2 - 1] + (double) array[array.length / 2]) / 2;
+        else
+            median = array[array.length / 2];
 
         Comparator<Integer> compareByMedian = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                if (Math.abs(o1 - 13) - Math.abs(o2 - 13) == 0)
+                if (Math.abs(o1 - median) - Math.abs(o2 - median) == 0)
                     return o1 - o2;
                 else
-                    return Math.abs(o1 - 13) - Math.abs(o2 - 13);
+                    return (int) (Math.abs(o1 - median) - Math.abs(o2 - median));
             }
         };
 
-        Collections.sort(list, compareByMedian);
+        Arrays.sort(array, compareByMedian);
 
-        array = new Integer[list.size()];
-        array = list.toArray(array);
         return array;
     }
 }
