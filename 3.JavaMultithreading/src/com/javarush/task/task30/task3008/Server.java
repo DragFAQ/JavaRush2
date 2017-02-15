d5b9cfd5d72701a78870fa1cd50406a8ef53e85e
Drag <drag.faq@gmail.com>
2017-02-15 21:06:32 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,33 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt());
+        ConsoleHelper.writeMessage("Сервер запущен.");
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                try {
+                    (new Handler(socket)).start();
+                } finally {
+                    socket.close();
+                }
+            }
+        } catch (Exception e){
+            serverSocket.close();
+            ConsoleHelper.writeMessage(e.getMessage());
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
