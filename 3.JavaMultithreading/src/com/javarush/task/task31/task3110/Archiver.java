60986dadf53ad5f6ca2e69ee6579d4401dc2f488
Drag <drag.faq@gmail.com>
2017-02-17 18:18:22 +0200

    Resolved

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String destFile = reader.readLine();
+        String sourceFile = reader.readLine();
+        reader.close();
+
+        if (!destFile.isEmpty() && !sourceFile.isEmpty()) {
+            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(destFile));
+            zipFileManager.createZip(Paths.get(sourceFile));
+        }
+    }
+}
