5359488ad9851a227bf5ce67434f3f67ebd6f132
Drag <drag.faq@gmail.com>
2017-02-18 11:49:40 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,13 +1,38 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        for (Operation operation : Operation.values())
+            ConsoleHelper.writeMessage(operation.ordinal() + " - " + operation);
+
+        return Operation.values()[ConsoleHelper.readInt()];
+    }
+
     public static void main(String[] args) throws Exception {
+        Operation operation;
+
+        while (true) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+                if (operation.equals(Operation.EXIT))
+                    break;
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }
+/*
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String destFile = reader.readLine();
         String sourceFile = reader.readLine();
@@ -18,6 +43,6 @@ public class Archiver {
             zipFileManager.createZip(Paths.get(sourceFile));
         }
 
-        (new ExitCommand()).execute();
+        (new ExitCommand()).execute();*/
     }
 }
