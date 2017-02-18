2b1b8728159a44f1c734579fecced0915c4f0281
Drag <drag.faq@gmail.com>
2017-02-18 11:08:51 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -10,7 +14,20 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        try (
+                ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+                InputStream inputStream = Files.newInputStream(source);
+        ) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
 
+
+            while (inputStream.available() > 0) {
+                int data = inputStream.read();
+                zipOutputStream.write(data);
+            }
+            zipOutputStream.closeEntry();
+        }
     }
 }
 
