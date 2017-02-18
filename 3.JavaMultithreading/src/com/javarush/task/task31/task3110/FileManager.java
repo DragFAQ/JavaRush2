e6103bff3650b71fccb1bd2a7c546cfe17bc344e
Drag <drag.faq@gmail.com>
2017-02-19 01:37:11 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -1,6 +1,5 @@
 package com.javarush.task.task31.task3110;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -14,7 +13,7 @@ public class FileManager {
 
     public FileManager(Path rootPath) throws IOException {
         this.rootPath = rootPath;
-        fileList = new ArrayList<>();
+        this.fileList = new ArrayList<>();
         collectFileList(rootPath);
     }
 
@@ -23,13 +22,21 @@ public class FileManager {
     }
 
     private void collectFileList(Path path) throws IOException {
-        if (Files.isRegularFile(path))
-            fileList.add(rootPath.relativize(path));
-        else if (Files.isDirectory(path))
+        // Добавляем только файлы
+        if (Files.isRegularFile(path)) {
+            Path relativePath = rootPath.relativize(path);
+            fileList.add(relativePath);
+        }
+
+        // Добавляем содержимое директории
+        if (Files.isDirectory(path)) {
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
             try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
-                for (Path dirFile : directoryStream) {
-                    collectFileList(dirFile);
+                for (Path file : directoryStream) {
+                    collectFileList(file);
                 }
-            } catch (IOException ex) {}
+            }
+        }
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,8 +1,12 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -14,19 +18,39 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        if (Files.notExists(zipFile.getParent()))
+            Files.createDirectories(zipFile.getParent());
+
         try (
                 ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
-                InputStream inputStream = Files.newInputStream(source);
         ) {
-            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            if (Files.isRegularFile(source))
+                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
+            else if (Files.isDirectory(source))
+            {
+                FileManager fileManager = new FileManager(source);
+                List<Path> fileNames = fileManager.getFileList();
+                for (Path p : fileNames)
+                    addNewZipEntry(zipOutputStream, source, p);
+            } else
+                throw new PathIsNotFoundException();
+        }
+    }
+
+    private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
+        try (InputStream inputStream = Files.newInputStream(filePath.resolve(fileName)))
+        {
+            ZipEntry zipEntry = new ZipEntry(fileName.toString());
             zipOutputStream.putNextEntry(zipEntry);
-
-
-            while (inputStream.available() > 0) {
-                int data = inputStream.read();
-                zipOutputStream.write(data);
-            }
+            copyData(inputStream, zipOutputStream);
             zipOutputStream.closeEntry();
+        }
+    }
+
+    private void copyData(InputStream in, OutputStream out) throws Exception {
+        while (in.available() > 0) {
+            int data = in.read();
+            out.write(data);
         }
     }
 }
