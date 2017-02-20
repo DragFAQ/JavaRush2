70f96df79a178cf83bb656637b7159ce5613da40
Drag <drag.faq@gmail.com>
2017-02-20 18:11:16 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
@@ -11,14 +11,14 @@ public class ZipAddCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         try {
-            ConsoleHelper.writeMessage("Добавление файлов в архив.");
+            ConsoleHelper.writeMessage("Добавление нового файла в архив.");
 
             ZipFileManager zipFileManager = getZipFileManager();
 
-            ConsoleHelper.writeMessage("Введите полный путь файла для добавления:");
+            ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
             Path sourcePath = Paths.get(ConsoleHelper.readString());
-            zipFileManager.addFile(sourcePath);
 
+            zipFileManager.addFile(sourcePath);
 
             ConsoleHelper.writeMessage("Добавление в архив завершено.");
 
