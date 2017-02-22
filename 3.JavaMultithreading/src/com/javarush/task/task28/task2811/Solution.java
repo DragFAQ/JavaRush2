8c79e184dd48fc0739e73ba2a17c34136b240238
Drag <drag.faq@gmail.com>
2017-02-22 14:52:43 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2811/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2811/Solution.java
@@ -4,6 +4,8 @@ package com.javarush.task.task28.task2811;
 ReentrantReadWriteLock
 */
 
+import java.util.LinkedHashMap;
+
 public class Solution {
     public static void main(String[] args) {
         ReadWriteMap<Integer, String> linkedSafeMap = new ReadWriteMap<>(new LinkedHashMap<>());
