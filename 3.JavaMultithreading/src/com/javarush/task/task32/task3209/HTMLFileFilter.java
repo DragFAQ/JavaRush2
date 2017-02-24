076893a7a097922042f5a1e9e8d8f9c88489d864
Drag <drag.faq@gmail.com>
2017-02-24 21:48:23 +0200

    Resolved

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/HTMLFileFilter.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+import java.nio.file.Files;
+
+public class HTMLFileFilter extends FileFilter {
+    @Override
+    public boolean accept(File f) {
+        return  (f.isDirectory()) ||
+                (f.getName().toUpperCase().lastIndexOf(".HTML") == f.getName().length() - 5) ||
+                (f.getName().toUpperCase().lastIndexOf(".HTM") == f.getName().length() - 4);
+    }
+
+    @Override
+    public String getDescription() {
+        return "HTML и HTM файлы";
+    }
+}
