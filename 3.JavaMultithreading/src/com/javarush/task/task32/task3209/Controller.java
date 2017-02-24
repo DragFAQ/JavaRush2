b2c4f26e669a04e04f82c016ac689805fb537dd5
Drag <drag.faq@gmail.com>
2017-02-24 21:25:02 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -73,5 +73,17 @@ public class Controller {
 
         return writer.toString();
     }
+
+    public void createNewDocument() {
+    }
+
+    public void openDocument() {
+    }
+
+    public void saveDocument() {
+    }
+
+    public void saveDocumentAs() {
+    }
 }
 
