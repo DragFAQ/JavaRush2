df324c7df3092718d01aac5eb2f3d2b1494bdedc
Drag <drag.faq@gmail.com>
2017-02-24 12:37:21 +0200

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/MenuHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/MenuHelper.java
@@ -13,6 +13,44 @@ public class MenuHelper {
     }
 
     public static JMenuItem addMenuItem(JMenu parent, Action action) {
+        JMenuItem item = new JMenuItem(action);
+        parent.add(item);
+
+        return item;
+    }
+
+    public static JMenuItem addMenuItem(JMenu parent, String text, Action action) {
+        JMenuItem item = addMenuItem(parent, action);
+        item.setText(text);
+
+        return item;
+    }
+
+    public static void initHelpMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initFontMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initColorMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initAlignMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initStyleMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initEditMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initFileMenu(View view, JMenuBar menuBar) {
 
     }
 }
