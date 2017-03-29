744a082feed4d47976fc69d15d8cc16f1ddf09a0
Drag <drag.faq@gmail.com>
2017-03-29 19:03:40 +0300

    Resolved

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
@@ -21,11 +21,23 @@ public class Solution {
         }
     }
 
+    public static String getAllNumbersBetween(int a, int b) {
+        String result = String.valueOf(a);
+        int inc = a < b ? 1 : -1;
+
+        while (a != b) {
+            a += inc;
+            result += " " + a;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         Random random = new Random();
         numberA = random.nextInt() % 1_000;
         numberB = random.nextInt() % 10_000;
-        System.out.println(recursion(numberA, numberB));
-        System.out.println(recursion(numberB, numberA));
+        System.out.println(getAllNumbersBetween(numberA, numberB));
+        System.out.println(getAllNumbersBetween(numberB, numberA));
     }
 }
